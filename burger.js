b9be526a9796696b2171f8ff58784d28c59cf172
Lxg1c <teran_2005@mail.ru>
2024-09-29 14:38:03 +0300

    first

---
+++ burger.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger').addEventListener('click', function () {
+	document.getElementById('header').classList.toggle('open')
+	document.getElementById('page').classList.toggle('dis-scroll')
+})
